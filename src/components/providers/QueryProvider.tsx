d27b03c20653dbc1e32fcf8b3d3c591e24052cc6
el-bana <ibrahimaelbana@gmail.com>
2026-04-05 02:46:00 +0200

    feat: implement TanStack Query integration and core Pokemon display components

---
+++ src/components/providers/QueryProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Ensure the query client is only created once per session
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000 * 5, // 5 minutes because our data don't change that much
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
