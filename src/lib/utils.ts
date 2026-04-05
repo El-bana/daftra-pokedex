242015c536930965a59b86e2145a9e2ed7b3df7b
el-bana <ibrahimaelbana@gmail.com>
2026-04-05 03:32:00 +0200

    feat: add cn utility for managing tailwind classes

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combines clsx and tailwind-merge for optimal class merging.
+ * @param inputs - Class names or conditional class values.
+ * @returns A single string with merged class names.
+ */
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
